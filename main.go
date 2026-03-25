80d12a9e5919eafcca36970a42769872860d760f
mahmoudrabbas <mahmoudrabass11@gmail.com>
2026-03-25 19:18:43 +0200

    first commit

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"example.com/events/models"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	server := gin.Default()
+	server.GET("/events", getEvents)
+	server.POST("/events", createEvent)
+
+	server.Run(":8080")
+}
+
+func getEvents(context *gin.Context) {
+	events := models.GetEvents()
+
+	context.JSON(http.StatusOK, gin.H{"events": events})
+
+}
+
+func createEvent(context *gin.Context) {
+	fmt.Println("D")
+	var event models.Event
+
+	err := context.ShouldBindJSON(&event)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"msg": err})
+		return
+	}
+	event.Id = 1
+	event.UserId = 1
+
+	event.Save()
+	context.JSON(http.StatusCreated, gin.H{"message": "Created Event.", "event": event})
+}
---
+++ models/event.go
@@ -0,0 +1,23 @@
+package models
+
+import "time"
+
+type Event struct {
+	Id          int
+	Name        string    `binding:"required"`
+	Description string    `binding:"required"`
+	Location    string    `binding:"required"`
+	DateTime    time.Time `binding:"required"`
+	UserId      int
+}
+
+var events []Event = []Event{}
+
+func (e *Event) Save() {
+	// save to db
+	events = append(events, *e)
+}
+
+func GetEvents() []Event {
+	return events
+}
