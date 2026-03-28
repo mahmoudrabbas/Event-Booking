f43c31a927bf1ddf328de7be253cd47c0f695477
mahmoudrabbas <mahmoudrabass11@gmail.com>
2026-03-28 23:06:06 +0200

    adding verify token

--- routes/events.go
+++ routes/events.go
@@ -6,6 +6,7 @@ import (
 	"strconv"
 
 	"example.com/events/models"
+	"example.com/events/utils"
 	"github.com/gin-gonic/gin"
 )
 
@@ -21,10 +22,26 @@ func getEvents(context *gin.Context) {
 }
 
 func createEvent(context *gin.Context) {
-	// fmt.Println("1")
+
+	token := context.Request.Header.Get("Authorization")
+
+	fmt.Println("token: ", token)
+
+	if token == "" {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": "Please Sign-in!"})
+		return
+	}
+
+	err := utils.VerifyToken(token)
+
+	if err != nil {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": err.Error()})
+		return
+	}
+
 	var event models.Event
 
-	err := context.ShouldBindJSON(&event)
+	err = context.ShouldBindJSON(&event)
 	if err != nil {
 		context.JSON(http.StatusBadRequest, gin.H{"message1": err})
 		return
--- utils/jwt.go
+++ utils/jwt.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"errors"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
@@ -16,5 +17,41 @@ func GenerateToken(email string, userId int64) (string, error) {
 	})
 
 	return token.SignedString([]byte(SECRET_KEY))
+
+}
+
+func VerifyToken(token string) error {
+
+	parsedToken, err := jwt.Parse(token, func(token *jwt.Token) (any, error) {
+
+		_, ok := token.Method.(*jwt.SigningMethodHMAC)
+
+		if !ok {
+			return nil, errors.New("Unexpected SigningMethod.")
+		}
+
+		return []byte(SECRET_KEY), nil
+	})
+
+	if err != nil {
+		return errors.New("Couldnt Parse The Token.")
+	}
+
+	claims, ok := parsedToken.Claims.(jwt.MapClaims)
+
+	if !ok {
+		return errors.New("Invalid Token Claims.")
+	}
+
+	expFloat := claims["exp"].(float64)
+
+	expInt := int64(expFloat)
+
+	if time.Now().Unix() > expInt {
+		return errors.New("Expired Token, Please Signin again.")
+
+	}
+
+	return nil
 
 }
